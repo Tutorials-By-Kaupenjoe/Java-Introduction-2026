12950cf7a7369697ba20cb1596b537ce611cb9a9
Kaupenjoe <N.Kaupenjohann@gmail.com>
2025-11-30 15:26:15 +0100

    3 data types

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,51 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        /* DATA TYPES & VARIABLES */
+
+        // Hello I am a comment
+        /*
+         * And I am a multiline Comment!
+         * I can come in quite handy for explaining stuff!
+         */
+
+        // Step 1: Read in File
+        // Step 2: Clean Up Input...
+
+        /* Variables */
+
+        // datatype name_of_variable; // Declaration
+        // OR
+        // datatype name_of_variable = value_of_variable; // Initialization (Declaration + Assignment of Value)
+
+        /* DATA TYPES */
+
+        // Integers (Store whole numbers (including negative ones))
+        // Go from -2147483648 to 2147483647
+        int points;
+        points = 100;
+        int x = -69;
+
+
+        // Floating Point Numbers (float & double)
+        float pi = 3.14159265f;
+        double e = 2.718281828;
+
+        float accountValue = 20.20f; // NEVER DO THIS!
+
+        // For real numbers, however! Precision can be an issue!
+        // Scientific notation: 314159265 * 10^-8
+
+
+        // Booleans (True or False)
+        boolean likedVideo = true;
+        boolean subscribed = false;
+
+
+        // Chars & Strings
+        char grade = 'A';
+        String name = "Kaupenjoe likes this video";
+
+
     }
 }
