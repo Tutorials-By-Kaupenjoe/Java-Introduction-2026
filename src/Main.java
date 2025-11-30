22096a11c103297c0f4d47ef71a4b402d356553b
Kaupenjoe <N.Kaupenjohann@gmail.com>
2025-11-30 16:38:32 +0100

    5 integer math

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,61 @@
-import java.util.Scanner;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        /* INPUT & OUTPUT */
+        /* INTEGERS & MATH */
 
-        /* OUTPUT */
-        // You (usually) output Strings
-        System.out.println("Hello World! This is our first output!");
+        int a = 100;
+        int b = 200;
 
-        // Values
-        int life = 42;
-        System.out.println(life);
+        /* ADDITION */
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
 
-        // Value and string
-        int grade = 85;
-        System.out.println("Your Grade is " + grade);
+        sum = 50;
+        System.out.println("Sum: " + sum);
 
-        /* INPUT */
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt(); // HAS TO BE A NUMBER!
-        System.out.println("You just typed " + number);
+        /* SUBTRACTION */
+        int sub = a - b;
+        System.out.println("Subtraction: " + sub);
 
-        String input = scanner.next();
-        System.out.println("You just typed (String): " + input);
+        /* MULTIPLICATION */
+        int multi = a * b;
+        System.out.println("Multiplication: " + multi);
 
+        /* DIVISION */
+        int division = a / b;
+        System.out.println("Division: " + division);
+
+        double x = 100;
+        double y = 200;
+        double doubleDivision = x / y;
+        System.out.println("Double Division: " + doubleDivision);
+
+        // No Division by 0
+        // division = b / 0;
+        // System.out.println("Division: " + division);
+
+        /* REMAINDER (MODULO) */
+        int remainder = 100 % 2; // = 0 => Even Number | = 1 => Uneven Number!
+        System.out.println("Remaining from 100 / 2: " + remainder);
+
+        remainder = 100 % 3;
+        System.out.println("Remaining from 100 / 3: " + remainder);
+
+        remainder = 100 % 4;
+        System.out.println("Remaining from 100 / 4: " + remainder);
+
+        /* MATH METHODS */
+        System.out.println("Rounding " + Math.round(6.4));       // Output: 6
+        System.out.println("Rounding Down " + Math.floor(500.2f));  // Output: 6
+        System.out.println("Rounding Up " + Math.ceil(6.4));    // Output: 7
+        System.out.println("Exponent " + Math.pow(2, 3));       // Output: 2^3 = 8
+
+        // More "Math Methods"
+        // Math.
+
+        // "RANDOM" Numbers
+        System.out.println("Random Number between 0 - 10: " + Math.random() * 10);
+        System.out.println("Random Number between 0 - 10: " + Math.round(Math.random() * 10));
     }
 }
