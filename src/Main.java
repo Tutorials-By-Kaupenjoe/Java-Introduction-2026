ce348a97b4b9d6e3d799570d194ada1dff1929bb
Kaupenjoe <N.Kaupenjohann@gmail.com>
2025-11-30 15:45:41 +0100

    4 input output

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,30 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        /* DATA TYPES & VARIABLES */
+        /* INPUT & OUTPUT */
 
-        // Hello I am a comment
-        /*
-         * And I am a multiline Comment!
-         * I can come in quite handy for explaining stuff!
-         */
+        /* OUTPUT */
+        // You (usually) output Strings
+        System.out.println("Hello World! This is our first output!");
 
-        // Step 1: Read in File
-        // Step 2: Clean Up Input...
+        // Values
+        int life = 42;
+        System.out.println(life);
 
-        /* Variables */
+        // Value and string
+        int grade = 85;
+        System.out.println("Your Grade is " + grade);
 
-        // datatype name_of_variable; // Declaration
-        // OR
-        // datatype name_of_variable = value_of_variable; // Initialization (Declaration + Assignment of Value)
+        /* INPUT */
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt(); // HAS TO BE A NUMBER!
+        System.out.println("You just typed " + number);
 
-        /* DATA TYPES */
-
-        // Integers (Store whole numbers (including negative ones))
-        // Go from -2147483648 to 2147483647
-        int points;
-        points = 100;
-        int x = -69;
-
-
-        // Floating Point Numbers (float & double)
-        float pi = 3.14159265f;
-        double e = 2.718281828;
-
-        float accountValue = 20.20f; // NEVER DO THIS!
-
-        // For real numbers, however! Precision can be an issue!
-        // Scientific notation: 314159265 * 10^-8
-
-
-        // Booleans (True or False)
-        boolean likedVideo = true;
-        boolean subscribed = false;
-
-
-        // Chars & Strings
-        char grade = 'A';
-        String name = "Kaupenjoe likes this video";
-
+        String input = scanner.next();
+        System.out.println("You just typed (String): " + input);
 
     }
 }
