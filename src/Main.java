af5fdb91b3c34fd48a0d715eb4ddfcb2a7915ad4
Kaupenjoe <N.Kaupenjohann@gmail.com>
2025-12-02 14:17:58 +0100

    9 if statements

--- src/Main.java
+++ src/Main.java
@@ -2,86 +2,48 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /* BOOLEANS & LOGIC */
+        /* CONTROL FLOW (IF & SWITCH) */
+        // "Everything is just an if statement"
+        // To "Control" the flow of the program, not just line by line!
 
-        // Booleans are either true or false
-        boolean goodVideo = true;
-        boolean subscribed = false;
-        System.out.println("This is a good video " + goodVideo);
-
-        boolean isCreative = false;
-        System.out.println("Player is in Creative Mode: " + isCreative);
-
-
-        /* COMPARISON (< <= >= > == !=) */
-        int points = 69;
-        int pointsForPassing = 55;
-        boolean hasPassedTest = points >= pointsForPassing;
-        System.out.println("Has passed Test: " + hasPassedTest);
-
-        int health = 0;
-        boolean isAlive = health > 0; // Health = 1 TRUE, Health = 0 False
-        System.out.println("Is Player Kaupenjoe still alive? " + isAlive);
-
-        // (VARIABLE/VALUE comp VARIABLE/VALUE) ==> BOOLEAN
-        // int + int ==> int
-
-        // BEWARE of STRINGS
         Scanner scanner = new Scanner(System.in);
-        String name = scanner.next();
-        boolean isKaupenjoe = name.equals("Kaupenjoe"); // .equals for Strings
+        System.out.println("How much Health you have?");
+        int health = scanner.nextInt();
+        boolean isAlive = health > 0;
 
-        System.out.println("Is Your Name Kaupenjoe? " + isKaupenjoe);
+        if(isAlive) {
+            System.out.println("You are still alive!");
+        } else {
+            System.out.println("Game Over: You have lost the game!");
+        }
 
+        System.out.println("How many points did you have in the exam?");
+        int points = scanner.nextInt();
 
-        /* LOGIC */
-        // "addition" and "subtraction" for Booleans
-        // (BOOLEAN logic BOOLEAN) ==> BOOLEAN
+        if(points >= 100) {
+            // If points are 100 or bigger
+            System.out.println("Passed with Honors");
+        } else if(points >= 50) {
+            // if points between 50 to 99
+            System.out.println("You passed!");
+        } else {
+            // if points below 50
+            System.out.println("You failed!");
+        }
 
-        // OR || ==> (a || b)
-        // a | b | c
-        // 0 | 0 | 0
-        // 1 | 0 | 1
-        // 0 | 1 | 1
-        // 1 | 1 | 1
+        /* SWITCH */
+        // for explicit action for cases
 
-        // AND && ==> (a && b)
-        // a | b | c
-        // 0 | 0 | 0
-        // 1 | 0 | 0
-        // 0 | 1 | 0
-        // 1 | 1 | 1
-
-        // NOT !
-        // a | !a
-        // 0 |  1
-        // 1 |  0
-
-        boolean passedTest1 = true;
-        boolean passedTest2 = true;
-        boolean hasHonors = passedTest1 && passedTest2;
-
-        // Mining Example
-        boolean hasPickaxe = true;
-        int durability = 5;
-        int neededDurability = 9;
-
-        boolean canMine3x3Area = (hasPickaxe && (durability >= neededDurability)) || isCreative;
-
-        /* DRINKING AGE EXAMPLE */
-        System.out.println("What is your age?");
-        int age = scanner.nextInt();
-        System.out.println("Put in true or false, are you from Europe?");
-        boolean isEuropean = scanner.nextBoolean();
-
-        int euDrinkingAge = 18;
-        int usDrinkingAge = 21;
-
-        boolean isAmerican = !isEuropean;
-        boolean couldDrinkInEU = age >= euDrinkingAge;
-        boolean couldDrinkInUS = age >= usDrinkingAge;
-
-        boolean canPersonDrinkAlcohol = isEuropean && couldDrinkInEU || isAmerican && couldDrinkInUS;
-        System.out.printf("Can you drink? " + canPersonDrinkAlcohol);
+        System.out.println("Gimme a Number between 0 and 5...");
+        int studentId = scanner.nextInt();
+        switch(studentId) {
+            case 0: System.out.println("Student Sarah has Id 0"); break;
+            case 1: System.out.println("Student Mara has Id 1"); break;
+            case 2: System.out.println("Student John has Id 2"); break;
+            case 3: System.out.println("Student Bowie has Id 3"); break;
+            case 4: System.out.println("Student Ludwig has Id 4"); break;
+            case 5: System.out.println("Student Nano has Id 5"); break;
+            default: System.out.println("ERROR: Student with id " + studentId + " not found!"); break;
+        }
     }
 }
