e38047316b68f5a4142c90eb508b1c07613d96b2
Kaupenjoe <N.Kaupenjohann@gmail.com>
2025-12-02 12:43:38 +0100

    8 booleans and logic

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,87 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        /* ASSIGNMENT OPERATORS */
+        /* BOOLEANS & LOGIC */
 
-        // Assignment =
-        int mana = 100;
-        System.out.println("Mana: " + mana);
-        // int x = 1000;
-        // x = mana;
+        // Booleans are either true or false
+        boolean goodVideo = true;
+        boolean subscribed = false;
+        System.out.println("This is a good video " + goodVideo);
 
-        // ADDITION +=
-        mana += 20; // mana = mana + 20;
-        System.out.println("Mana: " + mana);
-        mana++; // mana = mana + 1 // INCREMENT
-        System.out.println("Mana: " + mana); // 121
+        boolean isCreative = false;
+        System.out.println("Player is in Creative Mode: " + isCreative);
 
-        // SUBTRACTION -=
-        mana--; // mana = mana - 1 // DECREMENT
-        System.out.println("Mana: " + mana);
-        mana -= 60; // mana = mana - 60;
-        System.out.println("Mana: " + mana);
 
-        // MULTIPLY *=
-        mana *= 2;
-        System.out.println("Mana: " + mana);
+        /* COMPARISON (< <= >= > == !=) */
+        int points = 69;
+        int pointsForPassing = 55;
+        boolean hasPassedTest = points >= pointsForPassing;
+        System.out.println("Has passed Test: " + hasPassedTest);
 
-        // DIVIDE /=
-        mana /= 4;
-        System.out.println("Mana: " + mana);
+        int health = 0;
+        boolean isAlive = health > 0; // Health = 1 TRUE, Health = 0 False
+        System.out.println("Is Player Kaupenjoe still alive? " + isAlive);
 
-        // MODULO (REMAINDER) %=
-        mana %= 2;
-        System.out.println("Mana: " + mana);
+        // (VARIABLE/VALUE comp VARIABLE/VALUE) ==> BOOLEAN
+        // int + int ==> int
+
+        // BEWARE of STRINGS
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        boolean isKaupenjoe = name.equals("Kaupenjoe"); // .equals for Strings
+
+        System.out.println("Is Your Name Kaupenjoe? " + isKaupenjoe);
+
+
+        /* LOGIC */
+        // "addition" and "subtraction" for Booleans
+        // (BOOLEAN logic BOOLEAN) ==> BOOLEAN
+
+        // OR || ==> (a || b)
+        // a | b | c
+        // 0 | 0 | 0
+        // 1 | 0 | 1
+        // 0 | 1 | 1
+        // 1 | 1 | 1
+
+        // AND && ==> (a && b)
+        // a | b | c
+        // 0 | 0 | 0
+        // 1 | 0 | 0
+        // 0 | 1 | 0
+        // 1 | 1 | 1
+
+        // NOT !
+        // a | !a
+        // 0 |  1
+        // 1 |  0
+
+        boolean passedTest1 = true;
+        boolean passedTest2 = true;
+        boolean hasHonors = passedTest1 && passedTest2;
+
+        // Mining Example
+        boolean hasPickaxe = true;
+        int durability = 5;
+        int neededDurability = 9;
+
+        boolean canMine3x3Area = (hasPickaxe && (durability >= neededDurability)) || isCreative;
+
+        /* DRINKING AGE EXAMPLE */
+        System.out.println("What is your age?");
+        int age = scanner.nextInt();
+        System.out.println("Put in true or false, are you from Europe?");
+        boolean isEuropean = scanner.nextBoolean();
+
+        int euDrinkingAge = 18;
+        int usDrinkingAge = 21;
+
+        boolean isAmerican = !isEuropean;
+        boolean couldDrinkInEU = age >= euDrinkingAge;
+        boolean couldDrinkInUS = age >= usDrinkingAge;
+
+        boolean canPersonDrinkAlcohol = isEuropean && couldDrinkInEU || isAmerican && couldDrinkInUS;
+        System.out.printf("Can you drink? " + canPersonDrinkAlcohol);
     }
 }
