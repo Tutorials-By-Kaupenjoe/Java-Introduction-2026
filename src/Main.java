577041e3f088eb74807b6dcff476be45ce3fc6b3
Kaupenjoe <N.Kaupenjohann@gmail.com>
2025-12-03 16:18:50 +0100

    13 casting

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        /* TERNARY OPERATOR */
-        // A very "compact" way to do if-else
+        /* (BASIC) CASTING */
+        // Turning one Data Type into another
 
-        int health = 0;
-        String status = "";
-        System.out.println("What is your Status? " + status);
+        int health = 10;
+        float damage = 1.5f;
 
-        if(health > 0) {
-            status = "Alive";
-        } else {
-            status = "Non-Alive";
-        }
-        System.out.println("What is your Status? " + status);
-        status = "";
+        // int result = health - damage; // 10 - 1.5f = 8.5f
+        float resultFloat = health - damage; // yes, because it's a float!
+        System.out.println("Result: " + resultFloat); // 8.5
 
-        // TERNARY OPERATOR
-        // BOOLEAN ? TRUE_RETURN : FALSE_RETURN;
-        status = (health > 0) ? "Alive" : "Non-Alive";
-        System.out.printf("What is your status? " + status);
+        int resultInt = health - (int)damage; // explicit "Cast" damage from float to int
+        System.out.println("Result (int): " + resultInt); // 9? 8?
+        resultFloat = (float)health - damage; // 10 -> 10.0
+
+        resultInt = (int)((float)health - damage); // 8.5 to int --> 8
+        System.out.println("Result: " + resultInt);
+
+        String points = "100";
+        String pi = "3.14159265";
+
+        int result = Integer.parseInt(points);
+        System.out.println("Result: " + result);
     }
 }
