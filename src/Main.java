b9dc05e033255a8da3f9a15f3f9de76469aa188a
Kaupenjoe <N.Kaupenjohann@gmail.com>
2025-12-05 17:19:00 +0100

    21 access modifiers

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,30 @@ import net.kaupenjoe.Dog;
 
 public class Main {
     public static void main(String[] args) {
-        /* CLASSES & OBJECTS */
+        /* ACCESS MODIFIERS */
+
+        System.out.println("How many dogs are there: " + Dog.numberOfDogs);
 
         Dog bengie = new Dog("bengie.png", "Bengie", 11);
         Dog jeremy = new Dog("jeremy.jpg", "Jeremy", 7);
 
-        System.out.println("Bengie age: " + bengie.age);
-        System.out.println("Jeremy age: " + jeremy.age);
+        System.out.println("How many dogs are there: " + Dog.numberOfDogs);
+
+        System.out.println("Bengie age: " + bengie.getAge());
+        System.out.println("Jeremy age: " + jeremy.getAge());
 
         jeremy.bark();
         bengie.birthday();
 
-        System.out.println("Bengie age: " + bengie.age);
-        System.out.println("Jeremy age: " + jeremy.age);
+        System.out.println("Bengie age: " + bengie.getAge());
+        System.out.println("Jeremy age: " + jeremy.getAge());
 
-        bengie.age = 100; // modify this field directly! (not usually what we want)
+        // bengie.age = 100; // modify this field directly! (not usually what we want)
 
-        System.out.println("Bengie age: " + bengie.age);
+        System.out.println("Bengie age: " + bengie.getAge());
+
+        Dog charlie = new Dog("charlie.png", "Charlie", 13);
+
+        System.out.println("How many dogs are there: " + Dog.numberOfDogs);
     }
 }
--- src/net/kaupenjoe/Dog.java
+++ src/net/kaupenjoe/Dog.java
@@ -3,9 +3,9 @@ package net.kaupenjoe;
 public class Dog {
     // Members
     // Fields
-    public String picture;
-    public String name;
-    public int age;
+    public String picture;              // accessible from anywhere and changeable
+    protected final String name;        // accessible in sub-classes and same package; final => it can't be changed!
+    private int age;                    // accessible ONLY within this class
 
     // Constructors --> "Method" to create a new Instance of a Dog Object
     // Default Constructor
@@ -13,10 +13,15 @@ public class Dog {
 
     // }
 
+    public static int numberOfDogs = 0;
+
+
     public Dog(String picture, String name, int age) {
         this.picture = picture;
         this.name = name;
         this.age = age;
+
+        numberOfDogs++;
     }
 
     // Methods
@@ -26,5 +31,10 @@ public class Dog {
 
     public void bark() {
         System.out.println(name + " barked.");
+    }
+
+    // Getter
+    public int getAge() {
+        return age;
     }
 }
