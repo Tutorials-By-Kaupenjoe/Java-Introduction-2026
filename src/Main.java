18ee64506502d4359a5ed91ef4f80479c82dc5a4
Kaupenjoe <N.Kaupenjohann@gmail.com>
2025-12-05 20:28:54 +0100

    23 abstract class interfaces

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,26 @@
-import net.kaupenjoe.Dog;
+import net.kaupenjoe.*;
 
 public class Main {
     public static void main(String[] args) {
-        /* ACCESS MODIFIERS */
-
-        System.out.println("How many dogs are there: " + Dog.numberOfDogs);
+        /* ABSTRACT CLASSES & INTERFACES */
 
         Dog bengie = new Dog("bengie.png", "Bengie", 11);
         Dog jeremy = new Dog("jeremy.jpg", "Jeremy", 7);
+        Cat whiskers = new Cat("whiskers.png", "Whiskers", 4);
+        Animal john = new Cat("john.png", "John", 31); // new Animal doesn't work with Abstract Classes!
 
-        System.out.println("How many dogs are there: " + Dog.numberOfDogs);
+        Bird birdy = new Bird("birdy.jpg", "Birdy", 2);
+        Bird yoshi = new Bird("yoshi.jpg", "Yoshi", 3);
 
-        System.out.println("Bengie age: " + bengie.getAge());
-        System.out.println("Jeremy age: " + jeremy.getAge());
+        Bear browny = new Bear("browny.png", "Browny", 12);
 
-        jeremy.bark();
-        bengie.birthday();
+        Pettable nano = new Cat("nano.bmp", "Nano", 0);
 
-        System.out.println("Bengie age: " + bengie.getAge());
-        System.out.println("Jeremy age: " + jeremy.getAge());
+        PettingZoo myZoo = new PettingZoo(bengie, jeremy, whiskers, nano, (Cat)john);
+        myZoo.petRandomAnimal();
+        myZoo.petRandomAnimal();
+        myZoo.petRandomAnimal();
 
-        // bengie.age = 100; // modify this field directly! (not usually what we want)
 
-        System.out.println("Bengie age: " + bengie.getAge());
-
-        Dog charlie = new Dog("charlie.png", "Charlie", 13);
-
-        System.out.println("How many dogs are there: " + Dog.numberOfDogs);
     }
 }
--- src/net/kaupenjoe/Dog.java
+++ src/net/kaupenjoe/Dog.java
@@ -1,40 +1,30 @@
 package net.kaupenjoe;
 
-public class Dog {
-    // Members
-    // Fields
-    public String picture;              // accessible from anywhere and changeable
-    protected final String name;        // accessible in sub-classes and same package; final => it can't be changed!
-    private int age;                    // accessible ONLY within this class
-
-    // Constructors --> "Method" to create a new Instance of a Dog Object
-    // Default Constructor
-    // public Dog() {
-
-    // }
-
+public class Dog extends Animal implements Pettable {
     public static int numberOfDogs = 0;
 
-
     public Dog(String picture, String name, int age) {
-        this.picture = picture;
-        this.name = name;
-        this.age = age;
+        super(picture, name, age); // Basically the same as new Animal(...)
 
         numberOfDogs++;
     }
 
-    // Methods
-    public void birthday() {
-        age++;
+    @Override
+    public void move() {
+        System.out.println("Dog is running...");
     }
 
-    public void bark() {
-        System.out.println(name + " barked.");
+    @Override
+    public void makeSound() {
+        System.out.println(getName() + " woofed");
     }
 
-    // Getter
-    public int getAge() {
-        return age;
+    public void bellyRub() {
+        System.out.println("Belly rubbed of " + getName());
+    }
+
+    @Override
+    public void pet() {
+        bellyRub();
     }
 }
