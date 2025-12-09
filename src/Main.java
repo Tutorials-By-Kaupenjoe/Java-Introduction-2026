7eb22c325ae2c236e339bf71362ea3109aeba097
Kaupenjoe <N.Kaupenjohann@gmail.com>
2025-12-09 08:39:58 +0100

    27 enums

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,21 @@
-import net.kaupenjoe.*;
+import net.kaupenjoe.Difficulty;
+import net.kaupenjoe.ToolMaterial;
 
 public class Main {
     public static void main(String[] args) {
-        /* ANONYMOUS CLASSES */
+        /* Enums & Advanced Enums */
 
-        Dog bengie = new Dog("bengie.png", "Bengie", 11);
-        Dog jeremy = new Dog("jeremy.jpg", "Jeremy", 7);
-        Cat whiskers = new Cat("whiskers.png", "Whiskers", 4);
-        Animal john = new Cat("john.png", "John", 31); // new Animal doesn't work with Abstract Classes!
+        Difficulty gameDifficulty = Difficulty.MEDIUM;
+        System.out.println("Difficulty is " + gameDifficulty);
 
-        Bird birdy = new Bird("birdy.jpg", "Birdy", 2);
-        Bird yoshi = new Bird("yoshi.jpg", "Yoshi", 3) {
-            @Override
-            public void move() {
-                System.out.println(this.getName() + " soars through the skies.");
-            }
-        };
 
-        birdy.move();
-        yoshi.move();
+        ToolMaterial toolMaterial1 = ToolMaterial.STONE;
+        ToolMaterial toolMaterial2 = ToolMaterial.DIAMOND;
 
-        Bear browny = new Bear("browny.png", "Browny", 12);
+        toolMaterial1.useTool();
+        toolMaterial1.mine();
 
-        Pettable nano = new Cat("nano.bmp", "Nano", 0);
-        Pettable luc = new Pettable() {
-            @Override
-            public void pet() {
-                System.out.println("Luc looks weird at you, because he's a human...");
-            }
-        };
-
-        Animal july = new Animal("july.png", "July", 100) {
-            @Override
-            public void move() {
-                System.out.println(this.getName() + " crawls slowly.");
-            }
-        };
-
-        july.displayInfo();
-        july.move();
-
-        PettingZoo myZoo = new PettingZoo(bengie, jeremy, whiskers, nano, (Cat)john, luc);
-        myZoo.petRandomAnimal();
-        myZoo.petRandomAnimal();
-        myZoo.petRandomAnimal();
-
+        toolMaterial2.useTool();
+        toolMaterial2.mine();
     }
 }
---
+++ src/net/kaupenjoe/Difficulty.java
@@ -0,0 +1,8 @@
+package net.kaupenjoe;
+
+public enum Difficulty {
+    EASY,
+    MEDIUM,
+    HARD,
+    HARDCORE
+}
